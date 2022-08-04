4a521c5f498d7079048fefdfa9101be05371cb5c
yiyiwang <wang.yiyi1@northeastern.edu>
2022-08-04 10:15:21 -0700

    Added orders

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,30 @@
+class OrdersController < ApplicationController
+    before_action :find_meal, only: [:new, :create]
+
+    def new
+        @order = Order.new
+    end
+
+    def create
+        @order = Order.new(params.require(:order).permit(:recipient, :address))
+        @order.user = current_user
+        @order.meal = @meal
+
+        if @order.save
+            flash[:notice] = "Order created successfully!"
+            redirect_to user_path(current_user)
+        else
+            flash[:alert] = "Cannot create order!"
+            render "/orders/new", status: 303
+        end
+
+    end
+
+    def show
+    end
+
+    private
+    def find_meal
+        @meal = Meal.find(params[:meal_id])
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_03_220028) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_04_163939) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_03_220028) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_meals_on_user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.text "address"
+    t.string "recipient"
+    t.bigint "user_id", null: false
+    t.bigint "meal_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["meal_id"], name: "index_orders_on_meal_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -48,6 +59,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_03_220028) do
   end
 
   add_foreign_key "meals", "users"
+  add_foreign_key "orders", "meals"
+  add_foreign_key "orders", "users"
   add_foreign_key "reviews", "meals"
   add_foreign_key "reviews", "users"
 end
