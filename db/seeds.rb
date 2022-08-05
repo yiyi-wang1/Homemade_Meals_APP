94310e31b8466254be79cc40de2162fd6c6894c6
Xiaojun Lin <xiaojunlin@XiaojundeMacBook-Pro.local>
2022-08-04 17:45:18 -0700

    fixed 2 search bars

--- db/seeds.rb
+++ db/seeds.rb
@@ -81,7 +81,7 @@ users = User.all
     meal = Meal.create(
         title: Faker::Food.dish,
         description: Faker::Food.description,
-        ingredients: INGREDIENTSARR.sample(rand(2..10)).join(","),
+        ingredients: INGREDIENTSARR.sample(rand(2..10)).join(", "), # added a space after comma in join
         img_url: "https://loremflickr.com/640/640/meals?random=#{n + 1}",
         price: Faker::Commerce.price,
         user: users.sample
