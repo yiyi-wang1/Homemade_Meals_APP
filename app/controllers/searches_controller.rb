46a2567d238fec8b800a4d3f81c4f62d027cd6ac
yiyiwang <wang.yiyi1@northeastern.edu>
2022-08-05 10:43:08 -0700

    add controller and show page for ingredients

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,6 @@
 class SearchesController < ApplicationController
     def show
+        @keyword = params[:q].values[0]
         @q = Meal.ransack(params[:q])
         @meals = @q.result(distinct: true)
     end
